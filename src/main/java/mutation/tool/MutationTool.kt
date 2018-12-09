ead2151983dec8a80688c6598343636ee078a6a5
jcarlosadm <jcvf@ic.ufal.br>
2018-12-09 17:22:55 -0300

    add main class MutationTool

---
+++ src/main/java/mutation/tool/MutationTool.kt
@@ -0,0 +1,11 @@
+package mutation.tool
+
+import mutation.tool.util.MutationToolConfig
+
+class MutationTool(config:MutationToolConfig) {
+	val config = config
+	
+	fun run(){
+		
+	}
+}
---
+++ src/main/java/mutation/tool/util/MutationToolConfig.kt
@@ -0,0 +1,4 @@
+package mutation.tool.util
+
+class MutationToolConfig {
+}
