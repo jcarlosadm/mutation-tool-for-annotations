f0a4aa5ead182606c5e9933c51e53c9ea7f77f5f
jcarlosadm <jcvf@ic.ufal.br>
2018-12-09 18:08:15 -0300

    implement check project in run method

--- src/main/java/mutation/tool/MutationTool.kt
+++ src/main/java/mutation/tool/MutationTool.kt
@@ -1,11 +1,19 @@
 package mutation.tool
 
 import mutation.tool.util.MutationToolConfig
+import mutation.tool.project.Project
+import java.io.File
 
 class MutationTool(config: MutationToolConfig) {
 	val config = config
 
 	fun run() {
-
+		val project = Project(File(config.pathSources))
+		
+		// run original project against tests
+		if (project.runTests(config.pathTests) == false){
+			println("original project fail on tests. It doesn't seem right...")
+			return
+		}
 	}
 }
