0b5296de9c8b947bfc3139f483c0a092413cf8fe
jcarlosadm <jcvf@ic.ufal.br>
2018-12-09 21:38:16 -0300

    add log lib. this closes #2

--- src/main/java/mutation/tool/MutationTool.kt
+++ src/main/java/mutation/tool/MutationTool.kt
@@ -3,17 +3,28 @@ package mutation.tool
 import mutation.tool.util.MutationToolConfig
 import mutation.tool.project.Project
 import java.io.File
+import mu.KotlinLogging
+
+private val logger = KotlinLogging.logger{}
+private val TOOL_NAME = "Mutation Tool for Annotations"
 
 class MutationTool(config: MutationToolConfig) {
 	val config = config
 
 	fun run() {
+		logger.info { "\n\n	#### Starting $TOOL_NAME #### \n" }
+		
 		val project = Project(File(config.pathSources))
 		
-		// run original project against tests
+		logger.info { "Running original project against test suite" }
 		if (project.runTests(config.pathTests) == false){
-			println("original project fail on tests. It doesn't seem right... exiting...")
+			logger.error { "original project fail against test suite. exiting..." }
 			return
 		}
 	}
+}
+
+fun main(args: Array<String>) {
+	val config = MutationToolConfig("", "")
+	MutationTool(config).run()
 }
