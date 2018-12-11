add9c6de081616f278cf1a141d6a60f3892bd47c
jcarlosadm <jcvf@ic.ufal.br>
2018-12-11 02:41:15 -0300

    fix recursive call of setThreads on MutationToolConfig class

--- src/main/java/mutation/tool/util/MutationToolConfig.kt
+++ src/main/java/mutation/tool/util/MutationToolConfig.kt
@@ -5,6 +5,6 @@ import java.io.File
 class MutationToolConfig(val pathSources: File, val pathTests: File) {
 	var threads = 1
 		set(value) {
-			if (value > 0) threads = value
+			if (value > 0) field = value
 		}
 }
