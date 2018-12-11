7febb3dbdf09948a7744875638b4579c190075bc
jcarlosadm <jcvf@ic.ufal.br>
2018-12-11 20:33:45 -0300

    change MutationToolTest

--- src/test/java/mutation/tool/MutationToolTest.kt
+++ src/test/java/mutation/tool/MutationToolTest.kt
@@ -6,12 +6,24 @@ import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.Assertions.*
 import java.io.File
 
+private const val sourcePath = "./src/test/resources/fakeProject/src/main/java"
+private const val testPath = "./src/test/resources/fakeProject/src/test/java"
+
 internal class MutationToolTest {
 
     @Test
-    fun runWithoutTestMutants() {
-        val config = MutationToolConfig(File(""), File(""))
-        config.testMutants = false
-        assertTrue(MutationTool(config).run())
+    fun runPathSourceNotExists() {
+        assertTrue(File(testPath).exists())
+        val config = MutationToolConfig(File(""), File(testPath))
+        assertFalse(MutationTool(config).run())
     }
+
+    @Test
+    fun runPathTestNotExists() {
+        assertTrue(File(sourcePath).exists())
+        val config = MutationToolConfig(File(sourcePath), File(""))
+        assertFalse(MutationTool(config).run())
+    }
+
+
 }
---
+++ src/test/resources/fakeProject/src/test/java/MainTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions.*;
+
+public class MainTest {
+
+    @Test
+    public void test() {
+        assertTrue(true);
+    }
+}
