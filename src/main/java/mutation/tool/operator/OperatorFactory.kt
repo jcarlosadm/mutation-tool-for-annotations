a985b908f9f998911e5bc9b50169f4ad8b00b67d
J Carlos Viana F <jcvf@ic.ufal.br>
2019-01-25 15:04:47 -0300

    add CHODR to OperatorFactory

--- src/main/java/mutation/tool/operator/OperatorFactory.kt
+++ src/main/java/mutation/tool/operator/OperatorFactory.kt
@@ -11,12 +11,22 @@ class OperatorFactory(private val adaChecker: ADAChecker?) {
             OperatorsEnum.RMAT -> this.getRMATOperators(contexts, file)
             OperatorsEnum.ADA -> this.getADAOperators(contexts, file)
             OperatorsEnum.ADAT -> TODO()
-            OperatorsEnum.CHODR -> TODO()
+            OperatorsEnum.CHODR -> this.getCHODROperators(contexts, file)
             OperatorsEnum.RPA -> TODO()
             OperatorsEnum.RPAT -> TODO()
             OperatorsEnum.RPAV -> TODO()
             OperatorsEnum.SWTG -> TODO()
         }
+
+    private fun getCHODROperators(contexts: List<Context>, file: File): List<Operator> {
+        val operators = mutableListOf<Operator>()
+        for (context in contexts) {
+            val chodr = CHODR(context, file)
+            if (chodr.checkContext()) operators += chodr
+        }
+
+        return operators
+    }
 
     private fun getADAOperators(contexts: List<Context>, file: File): List<Operator> {
         if (this.adaChecker == null) return listOf()
