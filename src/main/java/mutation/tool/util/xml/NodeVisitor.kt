a2621dc475bc3ef933ee3d4ffb403b235282f430
jcarlosadm <jcvf@ic.ufal.br>
2019-10-07 18:45:50 -0300

    update project

--- src/main/java/mutation/tool/util/xml/NodeVisitor.kt
+++ src/main/java/mutation/tool/util/xml/NodeVisitor.kt
@@ -4,6 +4,8 @@ import org.w3c.dom.Node
 
 abstract class NodeVisitor {
 
+    protected lateinit var enterInfo:EnterInfo
+
     protected open fun visitClass(node: Node) {}
     protected open fun visitMethod(node: Node) {}
     protected open fun visitProperty(node: Node) {}
@@ -24,4 +26,31 @@ abstract class NodeVisitor {
             this.visit(node)
         }
     }
+
+    private fun visit(node: Node, enterInfo: EnterInfo) {
+        when(node.nodeName) {
+            NodeType.CLASS.nodeName -> {
+                enterInfo.enterClass = true
+                this.visitClass(node)
+            }
+            NodeType.METHOD.nodeName -> this.visitMethod(node)
+            NodeType.PARAMETER.nodeName -> this.visitParameter(node)
+            NodeType.PROPERTY.nodeName -> this.visitProperty(node)
+            NodeType.ATTRIBUTE.nodeName -> this.visitAttribute(node)
+        }
+
+        this.enterInfo = enterInfo
+
+        for (index in 0 until node.childNodes.length) {
+            val cnode = node.childNodes.item(index)
+            this.visit(cnode, enterInfo)
+        }
+    }
+}
+
+class EnterInfo {
+    var enterClass = false
+    var enterMethod = false
+    var enterParam = false
+    var enterProp = false
 }
