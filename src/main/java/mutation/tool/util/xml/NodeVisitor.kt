0b6453337829552289a4cb7db5d1e979a39f4293
J Carlos Viana F <jcvf@ic.ufal.br>
2019-10-07 20:32:00 -0300

    update project

--- src/main/java/mutation/tool/util/xml/NodeVisitor.kt
+++ src/main/java/mutation/tool/util/xml/NodeVisitor.kt
@@ -3,9 +3,6 @@ package mutation.tool.util.xml
 import org.w3c.dom.Node
 
 abstract class NodeVisitor {
-
-    protected lateinit var enterInfo:EnterInfo
-
     protected open fun visitClass(node: Node) {}
     protected open fun visitMethod(node: Node) {}
     protected open fun visitProperty(node: Node) {}
@@ -13,44 +10,62 @@ abstract class NodeVisitor {
     protected open fun visitAttribute(node: Node) {}
 
     fun visit(rootNode: Node) {
-        when(rootNode.nodeName) {
-            NodeType.CLASS.nodeName -> this.visitClass(rootNode)
-            NodeType.METHOD.nodeName -> this.visitMethod(rootNode)
-            NodeType.PARAMETER.nodeName -> this.visitParameter(rootNode)
-            NodeType.PROPERTY.nodeName -> this.visitProperty(rootNode)
-            NodeType.ATTRIBUTE.nodeName -> this.visitAttribute(rootNode)
-        }
-
-        for (index in 0 until rootNode.childNodes.length) {
-            val node = rootNode.childNodes.item(index)
-            this.visit(node)
-        }
+        this.visit(rootNode, false, false, false, false, false)
     }
 
-    private fun visit(node: Node, enterInfo: EnterInfo) {
+    private fun visit(
+            node: Node,
+            enterClass: Boolean,
+            enterMethod: Boolean,
+            enterProp: Boolean,
+            enterParam: Boolean,
+            enterAtrib: Boolean
+    ) {
+        val enter = EnterInfo()
+        enter.copy(enterClass,enterMethod,enterProp,enterParam,enterAtrib)
+
         when(node.nodeName) {
             NodeType.CLASS.nodeName -> {
-                enterInfo.enterClass = true
+                enter.enterClass = true
                 this.visitClass(node)
             }
-            NodeType.METHOD.nodeName -> this.visitMethod(node)
-            NodeType.PARAMETER.nodeName -> this.visitParameter(node)
-            NodeType.PROPERTY.nodeName -> this.visitProperty(node)
-            NodeType.ATTRIBUTE.nodeName -> this.visitAttribute(node)
+            NodeType.METHOD.nodeName -> {
+                enter.enterMethod = true
+                this.visitMethod(node)
+            }
+            NodeType.PARAMETER.nodeName -> {
+                enter.enterParam = true
+                this.visitParameter(node)
+            }
+            NodeType.PROPERTY.nodeName -> {
+                enter.enterProp = true
+                if (enter.enterClass && !enter.enterMethod) this.visitProperty(node)
+            }
+            NodeType.ATTRIBUTE.nodeName -> {
+                enter.enterAtrib = true
+                this.visitAttribute(node)
+            }
         }
 
-        this.enterInfo = enterInfo
-
         for (index in 0 until node.childNodes.length) {
-            val cnode = node.childNodes.item(index)
-            this.visit(cnode, enterInfo)
+            val childNode = node.childNodes.item(index)
+            this.visit(childNode, enter.enterClass,enter.enterMethod,enter.enterProp,enter.enterParam,enter.enterAtrib)
         }
     }
 }
 
-class EnterInfo {
+private class EnterInfo {
     var enterClass = false
     var enterMethod = false
     var enterParam = false
     var enterProp = false
+    var enterAtrib = false
+
+    fun copy(enterClass:Boolean, enterMethod:Boolean, enterProp:Boolean, enterParam:Boolean, enterAtrib:Boolean) {
+        this.enterClass = enterClass
+        this.enterMethod = enterMethod
+        this.enterProp = enterProp
+        this.enterParam = enterParam
+        this.enterAtrib = enterAtrib
+    }
 }
